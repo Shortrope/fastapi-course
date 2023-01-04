59d3e3a976bb375615abf332798889099452971a
Mario Kaack <mario@shortrope.com>
2023-01-03 22:01:30 -0800

    Create post, Update post now using SQLAlchemy
        modified:   app/main.py

--- app/main.py
+++ app/main.py
@@ -108,23 +108,16 @@ def delete_post(id: int, db: Session = Depends(get_db)):
 
 
 @app.put("/posts/{id}", status_code=status.HTTP_201_CREATED)
-def update_post(id: int, post: Post):
-    cursor.execute(
-        """UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *;""",
-        (
-            post.title,
-            post.content,
-            post.published,
-            id,
-        ),
-    )
-    updated_post = cursor.fetchone()
-    conn.commit()
-    if not updated_post:
+def update_post(id: int, post: Post, db: Session = Depends(get_db)):
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    original_post = post_query.first()
+    if not original_post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail=f"id: {id} not found!"
         )
-    return {"data": updated_post}
+    post_query.update(post.dict(), synchronize_session=False)
+    db.commit()
+    return {"data": post_query.first()}
 
 
 @app.get("/resetdb", status_code=status.HTTP_201_CREATED)
