d200d04dd0664c7dff91cf5e13772fc09e55beae
Mario Kaack <mario@shortrope.com>
2023-01-06 11:39:55 -0800

    Create UserCreate and UserResponse schemas
        modified:   app/schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -19,3 +19,19 @@ class PostResponse(PostBase):
 
     class Config:
         orm_mode = True
+
+
+class UserBase(BaseModel):
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserResponse(UserBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
